1098987e338d48f53655fb1a34829ddbd0aa9035
Fernando Puente <fernandopuente.es@gmail.com>
2018-08-14 17:41:09 +0200

    Subida paginas, voluntarios, inscribete, empresas, cookies, contacto + cambios

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+$(document).ready(function() {
+    
+    ///////////////////////////////////
+    // LISTA DESPLEGABLE
+    ///////////////////////////////////
+    
+    var lastitem='';
+    (function($) {
+
+    var allPanels = $('.desplegable > dd').hide();
+
+    $('.desplegable > dt > a').click(function() {
+        allPanels.slideUp();
+        if ($(this).text() != lastitem) {
+            $(this).parent().next().slideDown();
+            lastitem = $(this).text();
+        } else {
+            lastitem = '';
+        };
+        return false;
+      });
+
+    })(jQuery);
+    
+
+});
