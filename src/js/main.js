27a6cb7dd377f83d8ce88abf238873dd678f4fcf
Israel Gutiérrez Rojas <gootyfer@gmail.com>
2018-11-29 16:21:13 +0100

    Fix courses diagram navigation

--- src/js/main.js
+++ src/js/main.js
@@ -2,18 +2,29 @@
 
 console.log('>> Ready :)');
 
+function updateCalendarTitle(changeFromPagination){
+  var isFirstSlide = $(".slide-1").hasClass("swiper-slide-active");
+  if(changeFromPagination) isFirstSlide = !isFirstSlide;
+  if (isFirstSlide) {
+    $('.inscribete-calendario .date1').show();
+    $('.inscribete-calendario .date2').hide();
+  } else {
+    $('.inscribete-calendario .date2').show();
+    $('.inscribete-calendario .date1').hide();
+  }
+}
+
 $(document).ready(function() {
 
     //// CALENDARIO CURSOS
 
     $('.swiper-arrow').on('click', function(){
-        if ($(".slide-1").hasClass("swiper-slide-active")) {
-          $('.inscribete-calendario .date1').show();
-          $('.inscribete-calendario .date2').hide();
-        } else {
-          $('.inscribete-calendario .date2').show();
-          $('.inscribete-calendario .date1').hide();
-        }
+      updateCalendarTitle();
+    });
+    $('.swiper-pagination-bullet').on('click', function(){
+      if(!$(this).hasClass("swiper-pagination-bullet-active")){
+        updateCalendarTitle(true);
+      }
     });
 
     //// INICIAMOS MENSAJE COOKIES
