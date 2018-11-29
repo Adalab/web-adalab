314ee9a3fcc350045f7638f41db3078990363d62
Israel Gutiérrez Rojas <gootyfer@gmail.com>
2018-11-29 15:51:56 +0100

    Updates for the new business model and hackathon

--- docs/assets/js/main.min.js
+++ docs/assets/js/main.min.js
@@ -3,19 +3,21 @@
 console.log('>> Ready :)');
 
 $(document).ready(function() {
-    
+
     //// CALENDARIO CURSOS
-    
+
     $('.swiper-arrow').on('click', function(){
         if ($(".slide-1").hasClass("swiper-slide-active")) {
-          $('.inscribete-calendario .date').children('span').text('Octubre 2018');
+          $('.inscribete-calendario .date1').show();
+          $('.inscribete-calendario .date2').hide();
         } else {
-          $('.inscribete-calendario .date').children('span').text('Enero 2019');  
+          $('.inscribete-calendario .date2').show();
+          $('.inscribete-calendario .date1').hide();
         }
-    });  
-    
+    });
+
     //// INICIAMOS MENSAJE COOKIES
-    
+
     $('.cookie-message').cookieBar();
 });
 
