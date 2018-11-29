9c6c534e4c4b974f18bbe97f95df43697fb0ec43
Israel Gutiérrez Rojas <gootyfer@gmail.com>
2018-11-29 16:38:36 +0100

    Refactor swiper code

--- src/js/main.js
+++ src/js/main.js
@@ -2,28 +2,16 @@
 
 console.log('>> Ready :)');
 
-function updateCalendarTitle(changeFromPagination){
-  var isFirstSlide = $(".slide-1").hasClass("swiper-slide-active");
-  if(changeFromPagination) isFirstSlide = !isFirstSlide;
-  if (isFirstSlide) {
-    $('.inscribete-calendario .date1').show();
-    $('.inscribete-calendario .date2').hide();
-  } else {
-    $('.inscribete-calendario .date2').show();
-    $('.inscribete-calendario .date1').hide();
-  }
-}
-
 $(document).ready(function() {
 
     //// CALENDARIO CURSOS
-
-    $('.swiper-arrow').on('click', function(){
-      updateCalendarTitle();
-    });
-    $('.swiper-pagination-bullet').on('click', function(){
-      if(!$(this).hasClass("swiper-pagination-bullet-active")){
-        updateCalendarTitle(true);
+    swiper.on('slideChange', function () {
+      if (swiper.realIndex === 0) {
+        $('.inscribete-calendario .date1').show();
+        $('.inscribete-calendario .date2').hide();
+      } else {
+        $('.inscribete-calendario .date2').show();
+        $('.inscribete-calendario .date1').hide();
       }
     });
 
